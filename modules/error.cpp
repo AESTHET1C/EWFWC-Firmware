b4e7a1b2192a957f4e099a710440c39f2a91e567
Ａ Ｅ Ｓ Ｔ Ｈ Ｅ Ｔ １ Ｃ <tavaresa13@gmail.com>
2016-12-10 17:13:38 -0500

    Error module rewrite
    Dumbed down error module to only handle constant blinking.

--- modules/error.cpp
+++ modules/error.cpp
@@ -1,12 +1,10 @@
 #include "error.h"
 
-bool Error_Status[MACRO_ERROR_CODES];
+bool Error_Status;
 unsigned long Error_Tick_Start = 0;
-byte Error_Tick_Curr = 0;              // Current tick within the cycle (0-indexed)
-byte Error_Cycle_Blinks = 0;           // Number of blinks in the current cycle (1-indexed)
 
 void initErrors() {
-	clearErrors();
+	setError(false);
 	pinMode(ERROR_PIN, OUTPUT);
 	return;
 }
@@ -15,12 +13,7 @@ void handleErrorCodeDisplay() {
 	unsigned int Tick_Elapsed_Time = millis() - Error_Tick_Start;
 
 	if(Tick_Elapsed_Time >= ERROR_TICK_TIME) {
-		Error_Tick_Curr += 1;
-		if(Error_Tick_Curr == ERROR_CODES) {
-			Error_Tick_Curr = 0;
-			Error_Cycle_Blinks = getBlinksNext(Error_Cycle_Blinks);
-		}
-		if(Error_Tick_Curr < Error_Cycle_Blinks) {
+		if(Error_Status) {
 			digitalWrite(ERROR_PIN, HIGH);
 		}
 		Error_Tick_Start = millis();
@@ -31,38 +24,7 @@ void handleErrorCodeDisplay() {
 	return;
 }
 
-void flagError(byte error) {
-	if((error == 0) || (error > CRITICAL_ERROR)) {
-		return;
-	}
-	else if(error == CRITICAL_ERROR) {
-		clearErrors();
-	}
-	Error_Status[error - 1] = true;
+void setError(bool status) {
+	Error_Status = status;
 	return;
-}
-
-void clearErrors() {
-	for(byte Error = 0; Error < ERROR_CODES; Error++) {
-		Error_Status[Error] = false;
-	}
-	return;
-}
-
-byte getBlinksNext(byte blinks_prev) {
-	if(blinks_prev < ERROR_CODES) {
-		for(byte Blinks_Test = (blinks_prev + 1); Blinks_Test <= ERROR_CODES; Blinks_Test++) {
-			if(Error_Status[Blinks_Test - 1]) {
-				return Blinks_Test;
-			}
-		}
-	}
-	if(blinks_prev > 0) {
-		for(byte Blinks_Test = 1; Blinks_Test <= blinks_prev; Blinks_Test++) {
-			if(Error_Status[Blinks_Test - 1]) {
-				return Blinks_Test;
-			}
-		}
-	}
-	return 0;
 }
--- modules/error.h
+++ modules/error.h
@@ -1,26 +1,16 @@
-/* Error Code Module
+/* Error Module
  *
- * Used to initialize and control the status LED on the EWFWC hardware to show error codes
+ * Used to initialize and control the status LED on the EWFWC hardware to show an error state
  *
- * Error codes are displayed by blinking the corresponding number of times to the error code.
- * If multiple errors are set, each error is displayed in increasing order.
- * The highest error code is a critical error. If it is set, all other errors are cleared,
- * and the status LED blinks constantly. All Firmware operation should be halted if this happens.
+ * When enabled, an error code is displayed by constantly blinking the status LED.
  *
- * Each error code is displayed within a "cycle". Each cycle consists of ERROR_CODES "ticks".
- * Each tick lasts for ERROR_TICK_TIME milliseconds. At the beginning of each tick,
- * the LED may or may not turn on to represent a "blink".
- * After ERROR_BLINK_TIME elapses within a tick, the LED will be disabled.
- * The next cycle starts immediately afterwards, with no additional gap. As a result,
- * the error ERROR_CODES will constantly blink.
+ * Each error code blink is displayed within a "tick". Each tick lasts for ERROR_TICK_TIME milliseconds.
+ * Within each tick, the LED is enabled for the first ERROR_BLINK_TIME milliseconds.
  *
- * The following diagram assumes ERROR_CODES = 3 and the error code 2 is being displayed repeatedly:
- *
- * |_________           |_________           |                    |_________           |
- * |         |__________|         |_______________________________|         |__________| and so on...
+ * |_________           |_________           |
+ * |         |__________|         |__________| and so on...
  *  --blink--            --blink--
- *  --------tick-------- --------tick-------- --------tick--------
- * -----------------------------cycle-----------------------------
+ *  --------tick-------- --------tick--------
  *
  * Written by Alex Tavares <tavaresa13@gmail.com>
  */
@@ -33,10 +23,6 @@
 // CONFIGURATION VARIABLES
 /////////////////////////
 
-#define MACRO_ERROR_CODES 10
-
-const byte ERROR_CODES = MACRO_ERROR_CODES;
-const byte CRITICAL_ERROR = MACRO_ERROR_CODES;
 const unsigned int ERROR_TICK_TIME = 250;
 const unsigned int ERROR_BLINK_TIME = 100;
 
@@ -57,46 +43,25 @@ void initErrors();
  * Initializes error code handling
  * Must be called at startup
  *
- * Initialization involves setting status variables and pin configuration.
+ * Initialization involves setting status variable and pin configuration.
  *
- * Affects errors[], cycleStart, currentCycle, and currentCycleGoal
+ * Affects Error_Status
  */
 
 void handleErrorCodeDisplay();
 /*
- * Updates the EWMC status LED to display error codes
+ * Updates the EWFWC status LED to display error status
  * Must be placed within a loop that executes regularly
  *
- * Affects cycleStart, currentCycle, and currentCycleGoal
+ * Affects Error_Tick_Start
  */
 
-void flagError(byte error);
+void setError(bool status);
 /*
- * Sets a single error code to true
+ * Flags or clears error status
  *
- * Affects errors[]
- * INPUT:  Error code to set (1-indexed)
- */
-
-void clearErrors();
-/*
- * Clears all error codes
- *
- * Affects errors[]
- */
-
-
-/////////////////////////
-// INTERNAL FUNCTIONS
-/////////////////////////
-
-byte getBlinksNext(byte blinks_prev);
-/*
- * Scans through active errors and determines the next one to display
- * Used by handleErrorCodeDisplay()
- *
- * INPUT:  Previous displayed error (1-indexed)
- * OUTPUT: Next error to display (1-indexed)
+ * Affects Error_Status
+ * INPUT:  New error status
  */
 
 
